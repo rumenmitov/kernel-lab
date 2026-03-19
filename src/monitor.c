300cb746aed629d8b35e88f8b0e18a8ecc9a29cb
Rumen <rumenmitov@protonmail.com>
2026-03-19 17:04:25 +0100

    assignment 3

--- src/monitor.c
+++ src/monitor.c
@@ -10,19 +10,18 @@ static int interval = 5;
 module_param(interval, int, 0444);
 MODULE_PARM_DESC(interval, "How often, in seconds, to poll the process list.");
 
-static struct workqueue_struct *monitor_wq = NULL;
-static struct task_struct *monitor_task = NULL;
-struct work_struct monitor_work;
+static struct workqueue_struct *monitor_wq;
+static struct task_struct *monitor_task;
+static struct work_struct monitor_work;
+static bool monitor_loop_early_exit;
 
 /**
- * @brief Work item that counts processes.
+ * __monitor_count_procs() - Logs the current process count.
  */
-static void __monitor_work_handler(struct work_struct *work)
+static void __monitor_count_procs(void)
 {
 	struct task_struct *task;
 	uint32_t count = 0;
-
-	(void)work;
 
 	rcu_read_lock();
 	for_each_process(task) {
@@ -34,27 +33,77 @@ static void __monitor_work_handler(struct work_struct *work)
 }
 
 /**
- * @brief Every %interval seconds, add a work item to the workqueue.
- * @returns EINVAL on error.
+ * __monitor_work_handler() - Work item that counts processes.
+ */
+static void __monitor_work_handler(struct work_struct *work)
+{
+	(void)work;
+	__monitor_count_procs();
+}
+
+/**
+ * __monitor_loop() - Every %interval seconds, add a work item to the
+ * workqueue.
+ *
+ * Signal %SIGINT calls __monitor_count_procs() (i.e. the work item).
+ *
+ * Return:
+ * * 0 on success
+ * * %-EINVAL on error.
  */
 static int __monitor_loop(void *args)
 {
+	unsigned long msec_left;
+	bool sigint_rcvd = false;
+	bool sigterm_rcvd = false;
+
 	(void)args;
+
+	allow_signal(SIGINT);
+	allow_signal(SIGTERM);
 
 	INIT_WORK(&monitor_work, __monitor_work_handler);
 
 	while (!kthread_should_stop()) {
-		(void)msleep_interruptible(interval * 1000);
+		msec_left = msleep_interruptible(interval * 1000);
+
+		if (msec_left != 0 && signal_pending(current)) {
+			if (sigismember(&current->signal->shared_pending.signal,
+					SIGINT)) {
+				__monitor_count_procs();
+				sigint_rcvd = true;
+			}
+
+			if (sigismember(&current->signal->shared_pending.signal,
+					SIGTERM)) {
+				pr_info(KBUILD_MODNAME ": SIGTERM received\n");
+
+				sigterm_rcvd = true;
+			}
+
+			flush_signals(current);
+		}
+
+		if (sigterm_rcvd) {
+			monitor_loop_early_exit = true;
+			break;
+		}
+
+		if (sigint_rcvd) {
+			sigint_rcvd = false;
+			continue;
+		}
 
 		if (!monitor_wq) {
 			pr_err(KBUILD_MODNAME ": invalid workqueue!\n");
+			monitor_loop_early_exit = true;
 			return -EINVAL;
 		}
 
 		bool not_already_queued = queue_work(monitor_wq, &monitor_work);
-		if (!not_already_queued) {
+
+		if (!not_already_queued)
 			pr_warn(KBUILD_MODNAME ": work already queued\n");
-		}
 	}
 
 	return 0;
@@ -75,27 +124,35 @@ static int __init monitor_init(void)
 		return -EINVAL;
 	}
 
+	pr_info(KBUILD_MODNAME ": created workqueue\n");
+
 	monitor_task = kthread_run(__monitor_loop, NULL, "monitor_task");
 	if (IS_ERR(monitor_task)) {
 		pr_err(KBUILD_MODNAME ": failed to start monitor task!\n");
 		return PTR_ERR(monitor_task);
 	}
 
+	pr_info(KBUILD_MODNAME ": started monitor task\n");
+
 	return 0;
 }
 
 static void __exit monitor_exit(void)
 {
-	if (monitor_task) {
+	if (monitor_task && !monitor_loop_early_exit) {
 		(void)kthread_stop(monitor_task);
 		monitor_task = NULL;
 	}
+
+	pr_info(KBUILD_MODNAME ": stopped monitor task\n");
 
 	if (monitor_wq) {
 		flush_workqueue(monitor_wq);
 		destroy_workqueue(monitor_wq);
 		monitor_wq = NULL;
 	}
+
+	pr_info(KBUILD_MODNAME ": stopped workqueue\n\n");
 }
 
 module_init(monitor_init);
